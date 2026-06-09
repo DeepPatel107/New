f0633232d48723409113dcb6e8d6ae95003de0b6
Deep Patel <deeppatel23186@gmail.com>
2026-06-09 16:20:46 +0530

    Written boioler plate code

---
+++ A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // your code here
+
+    return 0;
+}
